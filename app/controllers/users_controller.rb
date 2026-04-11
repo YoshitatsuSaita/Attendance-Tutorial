8d38486fc135eb24f8fc5da2dd31c1f91d1ee1cc
YoshitatsuSaita <ysaita1215@gmail.com>
2026-04-11 20:59:04 +0900

    10.1 fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,7 @@ class UsersController < ApplicationController
       format.html { redirect_to users_url }
       format.turbo_stream
     end
-    
+    redirect_to users_url
   end
   
   private
